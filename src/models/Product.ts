1c860875477364ecbfc2d16f48a8f16afc445a7e
srilatha2012 <srilathp2012@gmail.com>
2026-04-09 13:51:41 -0400

    Rename product.ts to Product.ts

---
+++ src/models/Product.ts
@@ -0,0 +1,25 @@
+export class Product {
+    id: number;
+    title: string;
+    price: number;
+    discountPercentage: number;
+    sku: string;
+
+    //constructor to initialize product details
+    constructor(id: number, title: string, price: number, discountPercentage: number, sku: string) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.discountPercentage = discountPercentage;
+        this.sku = sku;
+    }
+    //Returns product details as a formatted string
+    displayDetails(): string {
+        return `ID: ${this.id}, Title: ${this.title}, Price: ${this.price}, DiscountPercentage: ${this.discountPercentage}, SKU: ${this.sku} `;
+    }
+    
+    //Returna product price with discount
+    getPriceWithDiscount(): number {
+      return this.price;
+    }
+}
